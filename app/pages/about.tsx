bf31b3959bed992c8e03645db45a2b48b58d9082
murata0531 <coless115@gmail.com>
2021-12-29 00:00:19 +0900

    styled-jsxを追加

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -1,15 +1,32 @@
+import type { ReactElement } from 'react';
+
 import Head from 'next/head'
 import Link from 'next/link'
 import { Layout } from '../components/Layout'
 
-export default () => (
-  <Layout>
-    <Head>
-      <title>About me</title>
-    </Head>
-    <h1>About me</h1>
-    <Link href="/">
-      <a>Back to home</a>
-    </Link>
-  </Layout>
-)
+function about() {
+    return <>
+        <Layout>
+        <Head>
+          <title>About me</title>
+        </Head>
+        <div className="body">
+            <h1>About me</h1>
+            <Link href="/">
+            <a id="back-link">Back to home</a>
+            </Link>
+        </div>
+      </Layout>
+      <style jsx>{`
+      .body{
+          height:100%;
+          background:red;
+      }
+      #back-link {
+        background: red;
+      }
+    `}</style>
+    </>
+  }
+  
+  export default about;
